b27ee0668f066e48f4d294e0a8b26ced95705de5
Sparsh Handa <sparshhanda2002@gmail.com>
2023-08-20 22:11:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use("/api/users", userRoute);
 app.use("/api/songs", songsRoute);
 app.use("/api/admin", adminRoute);
 
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = 5000;
 
 app.listen(port, () => console.log(`Node js server started at port ${port}!`));
